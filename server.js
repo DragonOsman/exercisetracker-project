82a401538d22257edff98355a0712fb91cb8e11f
Osman Zakir <osmanzakir90@hotmail.com>
2021-03-12 23:37:56 +0500

    set path to styles.css back to just filename without path and added some code to server.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}`, "/views/index.html"));
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
+app.post("/api/exercise/new-user", (req, res) => {
+  console.log(`${req.body}`);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
