2c5c077353255ab64141f727da4308a77cb05204
Osman Zakir <osmanzakir90@hotmail.com>
2021-04-07 16:43:30 +0500

    changed line 275 to use template string for message passed to console.log

--- server.js
+++ server.js
@@ -272,5 +272,5 @@ app.get("/api/exercise/log", (req, res) => {
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
